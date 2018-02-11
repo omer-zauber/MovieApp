6bdcc35979a1beac59de5c67f7de1ac667c60bbf
omer-zauber <omer.zauber@gmail.com>
2018-02-11 14:31:56 +0200

    heroku try: changed default port

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const { Movie } = require('./models/movie');
 const { calculateAverage } = require('./utils/calculateAverage');
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = 3001;
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'client/build')));
