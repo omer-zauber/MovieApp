df5abc9a0ba1c9ff2b656adf3894e12daeed211a
omer-zauber <omer.zauber@gmail.com>
2018-02-12 09:59:54 +0200

    without code from github

--- server/server.js
+++ server/server.js
@@ -12,9 +12,9 @@ const PORT = /*process.env.PORT ||*/ 5000;
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'client/build')));
 //
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.get('/*', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 //addition from github
 
 app.post('/api/movies', (req, res)=> {
